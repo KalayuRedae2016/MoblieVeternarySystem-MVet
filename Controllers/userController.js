09de2ded72a3f01a863d95b7b04a1819813a1eed
KalayuRedae2016 <kalayuredae2@gmail.com>
2025-07-12 15:43:10 +0300

    change bcryptjs

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 const { Animal, User, MedicalVisit } = require('../Models');
 const { Op, where } = require('sequelize');
