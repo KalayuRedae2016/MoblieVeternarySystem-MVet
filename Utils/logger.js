730bfdac4db8b123a646ee4f7140ad267abc7ad2
KalayuRedae2016 <kalayuredae2@gmail.com>
2025-07-13 08:15:39 +0300

    llogg

--- Utils/logger.js
+++ Utils/logger.js
@@ -9,7 +9,7 @@ const logger = winston.createLogger({
   transports: [
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
     new winston.transports.File({ filename: 'logs/combined.log' }),
-    new winston.transports.Console() // Will appear in pm2 logs
+    new winston.transports.Console() // Will appear in pm2 logs and console
   ],
 });
 
